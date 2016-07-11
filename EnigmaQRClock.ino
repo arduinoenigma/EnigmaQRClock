a660f46b35cd851de51bef0d5b5d30bb10529866
Arduino Enigma <arduinoenigma@gmail.com>
2016-07-10 21:51:57 -0400

    Add files via upload

---
+++ EnigmaQRClock.ino
@@ -0,0 +1,117 @@
+// A QR Clock for the Arduino UNO + Seeed Touchscreen LCD platform
+// by @arduinoenigma 
+// uses a modified @chooftech QR library and a modified Xark display driver
+// update times of a second are possible
+// http://ch00ftech.com/2013/04/23/optimizing-the-qr-clock/
+// http://ch00ftech.com/wp-content/uploads/2012/10/main1.c
+//
+// to set the time, open a serial monitor and type the following command
+// r1;h15;m22;s25;u10;d;x;
+// r1 sets rotation so plug is to the right. Any rotation is fine, setting is for purists
+// h15 sets hours to 15
+// m22 sets minutes to 22
+// s10 sets seconds to 25
+// u10 sets update rate to 10 seconds, 1,5,10,15,20,30,60 are valid
+// d shows the time
+// x starts the clock now
+// if a command with a two digit parameter is sent with two digits, the semicolon can be omitted
+// the following are valid strings
+// r1h15m22s25u10dx
+// r1h9;m5;s25u1;dx
+// 
+
+#define TFTSeed
+//#define TFTAdafruit
+
+#include <SPI.h>
+#include <FastTftILI9341.h>      //https://github.com/arduinoenigma/FastSeedTFTv2
+
+PDQ_ILI9341 Tft;
+
+//raw input string from user GLOBAL VAR to generate();
+char input[17];
+
+//GLOBAL VAR to generate() and printcode();
+unsigned char outputmatrix[56] = {128, 63, 192, 247, 247, 137, 254, 34, 209, 95, 36, 250, 139, 124, 127, 31, 160, 10, 248, 255, 255, 191, 255, 255, 255, 255, 255, 254, 255, 255, 255, 255, 251, 255, 255, 253, 63, 224, 255, 247, 253, 255, 162, 255, 95, 244, 255, 139, 254, 127, 223, 255, 15, 248, 255, 255};
+
+unsigned long ul_LastGenerated = 0UL;
+
+byte hours = 0;
+byte mins = 0;
+byte secs = 0;
+byte ampm = 0;
+byte update = 10;
+byte rotation = 0;
+
+byte xo = 15;
+byte yo = 55;
+
+void advancetime()
+{
+  input[0] = (hours / 10) + '0';
+  input[1] = (hours - (hours / 10) * 10) + '0';
+  input[2] = ':';
+  input[3] = (mins / 10) + '0';
+  input[4] = (mins - (mins / 10) * 10) + '0';
+
+  //input[5] = ';';
+  //input[6] = 0;
+
+  input[5] = ':';
+  input[6] = (secs / 10) + '0';
+  input[7] = (secs - (secs / 10) * 10) + '0';
+  input[8] = ';';
+  input[9] = 0;
+
+
+  secs += update;
+  if (secs >= 60)
+  {
+    secs = secs - 60;
+    mins++;
+    if (mins == 60)
+    {
+      mins = 0;
+      hours++;
+      if (hours == 24)
+      {
+        mins = 0;
+        hours = 0;
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Tft.TFTinit();
+
+  Serial.println("valid init string is: r1;h15;m22;s25;u10;d;x;");
+
+  advancetime();
+  generate();
+  ul_LastGenerated = millis();
+  printcode(&outputmatrix[0]);
+
+  advancetime();
+  generate();
+}
+
+void loop() {
+
+  unsigned long ul_CurrentMillis;
+  ul_CurrentMillis = millis();
+
+  SerialFSM();
+
+  if ((ul_CurrentMillis - ul_LastGenerated) >= (update * 1000))
+  {
+    ul_LastGenerated += update * 1000;
+    printcode(&outputmatrix[0]);
+    advancetime();
+    generate();
+  }
+
+
+
+}
---
+++ SerialMenu.ino
@@ -0,0 +1,192 @@
+byte numberstocollect = 0;
+byte storenumbersin = 0;
+byte collectednumber = 0;
+byte finishedcollection = 1;
+
+
+//to setup r1h15m22s10u10dx
+//bug: h1m1s1 does not work
+
+void SerialFSM()
+{
+
+  char KeyPressed = 0;
+
+  if (Serial.available())
+  {
+    KeyPressed = Serial.read();
+
+    switch (KeyPressed)
+    {
+        //set hours
+      case 'H':
+      case 'h':
+        {
+          collectednumber = 0;
+          numberstocollect = 2;
+          storenumbersin = 1;
+          break;
+        }
+
+        //set minutes
+      case 'M':
+      case 'm':
+        {
+          collectednumber = 0;
+          numberstocollect = 2;
+          storenumbersin = 2;
+          break;
+        }
+
+        //set seconds
+      case 'S':
+      case 's':
+        {
+          collectednumber = 0;
+          numberstocollect = 2;
+          storenumbersin = 3;
+          break;
+        }
+
+        //display AM/PM or 24hr
+      case 'P':
+      case 'p':
+        {
+          collectednumber = 0;
+          numberstocollect = 1;
+          storenumbersin = 4;
+          break;
+        }
+
+        //update every x seconds
+      case 'U':
+      case 'u':
+        {
+          collectednumber = 0;
+          numberstocollect = 2;
+          storenumbersin = 5;
+          break;
+        }
+
+        // set rotation
+      case 'R':
+      case 'r':
+        {
+          collectednumber = 0;
+          numberstocollect = 1;
+          storenumbersin = 6;
+          break;
+        }
+
+        // draw the time
+      case 'D':
+      case 'd':
+        {
+          Serial.print(hours);
+          Serial.print(":");
+          Serial.print(mins);
+          Serial.print(":");
+          Serial.println(secs);
+          break;
+        }
+
+        // start clock
+      case 'X':
+      case 'x':
+        {
+          ul_LastGenerated = millis();
+          advancetime();
+          advancetime();
+          generate();
+          break;
+        }
+
+        // collect numeric parameter or ; as separator
+      default:
+        {
+          if (numberstocollect)
+          {
+            finishedcollection = 0;
+            if ((KeyPressed >= '0') && (KeyPressed <= '9'))
+            {
+              collectednumber = collectednumber * 10 + (KeyPressed - '0');
+              numberstocollect--;
+              if (numberstocollect == 0)
+              {
+                finishedcollection = 1;
+              }
+            }
+            if (KeyPressed == ';')
+            {
+              finishedcollection = 1;
+            }
+          }
+
+          if (finishedcollection)
+          {
+            finishedcollection = 0;
+
+            switch (storenumbersin)
+            {
+              case 1:
+                {
+                  hours = collectednumber;
+                  break;
+                }
+              case 2:
+                {
+                  mins = collectednumber;
+                  break;
+                }
+              case 3:
+                {
+                  secs = collectednumber;
+                  break;
+                }
+              case 4:
+                {
+                  ampm = collectednumber;
+                  break;
+                }
+              case 5:
+                {
+                  if ((collectednumber == 1) || (collectednumber == 5) || (collectednumber == 10) || (collectednumber == 15) || (collectednumber == 20) || (collectednumber == 30) || (collectednumber == 60))
+                  {
+                    update = collectednumber;
+                  }
+                  break;
+                }
+              case 6:
+                {
+                  if (collectednumber < 4)
+                  {
+                    rotation = collectednumber;
+                    
+                    if ((rotation==0) || (rotation==2))
+                    {
+                      xo = 15;
+                      yo = 55;
+                    }
+                    else
+                    {
+                      yo = 15;
+                      xo = 55;
+                    }
+                    
+                    Tft.setRotation(rotation);
+                    Tft.fillScreen(BLACK);
+                    printcode(&outputmatrix[0]);
+                  }
+                  break;
+                }
+            }
+
+          }
+
+          break;
+        }
+    }
+
+
+  }
+}
